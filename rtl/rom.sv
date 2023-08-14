9e0fbe77dd203a1bddd5a7b01ab615b05777e36a
Ernesto Sola-Thomas <93721212+esola-thomas@users.noreply.github.com>
2023-08-14 15:47:20 +0000

    update rom logic, this is at test state

--- rtl/rom.sv
+++ rtl/rom.sv
@@ -1,19 +1,25 @@
-module rom # (
-    parameter addr_width = 32;
-    parameter data_width = 32;
+module rom #(
+    parameter addr_width = 32,
+    parameter data_width = 32
 )(
     input   [addr_width-1:0] addr_i,
     output  [data_width-1:0] data_o
 );
 
-// Declare registers that will store data from ROM
-reg [data_width-1:0] rom_data [0:(2**addr_width)-1]; 
+    // Declare registers that will store data from ROM
+    reg [data_width-1:0] rom_data [0:(2**addr_width)-1];
 
-initial begin
-    // Declare here the base configuration of the ROM 
-    // TODO find a way to import this from CSV or something similar
-end
+    generate
+        for (genvar i = 0; i < (2**addr_width); i++) begin : INIT_ROM
+            initial begin
+                // Initialize rom_data[i] based on your requirements
+                rom_data[i] = 32'h12345678;
+            end
+        end
+    endgenerate
 
-assign data = rom_data[addr_i];
+    always @(*) begin
+        data_o = rom_data[addr_i];
+    end
 
 endmodule
