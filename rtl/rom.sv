78406707255003d4f7a918025600224bae5d8f04
Ernesto Sola-Thomas <93721212+esola-thomas@users.noreply.github.com>
2023-08-20 16:47:59 +0000

    Add support for ROM init from mem file

--- rtl/rom.sv
+++ rtl/rom.sv
@@ -1,6 +1,6 @@
 module rom #(
-    parameter addr_width = 32,
-    parameter data_width = 32
+    parameter addr_width = 4,
+    parameter data_width = 16
 )(
     input   [addr_width-1:0] addr_i,
     output  [data_width-1:0] data_o
@@ -8,15 +8,7 @@ module rom #(
 
     // Declare registers that will store data from ROM
     reg [data_width-1:0] rom_data [0:(2**addr_width)-1];
-
-    generate
-        for (genvar i = 0; i < (2**addr_width); i++) begin : INIT_ROM
-            initial begin
-                // Initialize rom_data[i] based on your requirements
-                rom_data[i] = 32'h12345678;
-            end
-        end
-    endgenerate
+    $readmemh("init_rom.mem", rom_data);
 
     always @(*) begin
         data_o = rom_data[addr_i];
