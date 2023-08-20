a7be4a59704dc70c47adc6f56e3180b885215d30
Ernesto Sola-Thomas <93721212+esola-thomas@users.noreply.github.com>
2023-08-20 16:49:19 +0000

    Add error signal

--- rtl/rom.sv
+++ rtl/rom.sv
@@ -3,15 +3,31 @@ module rom #(
     parameter data_width = 16
 )(
     input   [addr_width-1:0] addr_i,
-    output  [data_width-1:0] data_o
+    output  [data_width-1:0] data_o,
+    output  error
 );
 
     // Declare registers that will store data from ROM
     reg [data_width-1:0] rom_data [0:(2**addr_width)-1];
-    $readmemh("init_rom.mem", rom_data);
+    reg read_success;
+
+    // Read memory initialization file and handle errors
+    initial begin
+        read_success = $readmemh("init_rom.mem", rom_data);
+        if (!read_success) begin
+            $display("Error: Unable to read memory initialization file.");
+            error = 1;
+        end
+    end
 
     always @(*) begin
-        data_o = rom_data[addr_i];
+        if (read_success && addr_i < (2**addr_width)) begin
+            data_o = rom_data[addr_i];
+            error = 0; // No error
+        end else begin
+            data_o = {data_width{1'bx}}; // Invalid data
+            error = 1; // Error
+        end
     end
 
 endmodule
