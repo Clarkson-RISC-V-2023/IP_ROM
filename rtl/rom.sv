ca57ff8ff6063586bf0c8533036b6b70b0d4e1da
Ernesto Sola-Thomas <93721212+esola-thomas@users.noreply.github.com>
2023-08-22 21:24:51 +0000

    New ROM Architecture, address increments by 1

--- rtl/rom.sv
+++ rtl/rom.sv
@@ -1,38 +1,27 @@
-// iverilog -o out/rom_rtl -s rom rom/rtl/rom.sv -y ./rom
+// TODO does the ROM address increase on increments of 1 or 16? Right now set as increments of 1
 module rom #(
-    parameter data_width = 16,
-    parameter addr_width = 5 * data_width
+    parameter DATA_WIDTH = 16,      // Word length
+    parameter ADDR_WIDTH = 8,       // Addr length
+    parameter WORDS = 5,            // Words
+    parameter ENABLE_ROM_INIT = 1   // Default: No initialization
+
 )(
-    input           [addr_width-1:0] addr_i,
-    output  reg     [data_width-1:0] data_o,
-    output  wire    error
+    input wire [ADDR_WIDTH-1:0] addr_i,
+    output reg [DATA_WIDTH-1:0] data_o
 );
-
-    // Declare registers that will store data from ROM
-    reg [data_width-1:0] rom_data [0:(2**addr_width)-1];
-    reg read_success;
-    reg error_reg;
-    assign error = error_reg;
-
-    // Read memory initialization file and handle errors
+    
+    
+    reg [DATA_WIDTH-1:0] memory [0:WORDS-1]; // 5 words of 16 bits each
+    
+    // ROM initialization block
     initial begin
-        read_success = $readmemh("init_rom.mem", rom_data);
-        if (!read_success) begin
-            $display("Error: Unable to read memory initialization file.");
-            error_reg = 1'b1; // Set initial value
-        end else begin
-            error_reg = 1'b0; // Set initial value
+        if (ENABLE_ROM_INIT) begin
+            $readmemh("rom_contents.hex", memory);
         end
     end
 
+    // ROM logic here
     always @(*) begin
-        if (read_success && $unsigned(addr_i) < (2**addr_width)) begin
-            data_o = rom_data[addr_i];
-            error_reg = 1'b0; // No error
-        end else begin
-            data_o = {data_width{1'bx}}; // Invalid data
-            error_reg = 1'b1; // Error
-        end
+        data_o = memory[addr_i];
     end
-
 endmodule
--- verif/tb_rom.sv
+++ verif/tb_rom.sv
@@ -1,16 +1,29 @@
 `timescale 1ns/1ps
-
+ 
 module tb_rom # (
-    parameter data_width = 16,
-    parameter addr_width = 5 * data_width
+    parameter DATA_WIDTH = 16,      // Word length
+    parameter ADDR_WIDTH = 8,       // Addr length
+    parameter WORDS = 5,            // Words
+    parameter ENABLE_ROM_INIT = 0   // Default: No initialization
 );
-    reg [addr_width-1:0] address; // Change 'wire' to 'reg'
-    wire [data_width-1:0] data;
+    reg [ADDR_WIDTH-1:0] address; // Change 'wire' to 'reg'
+    wire [DATA_WIDTH-1:0] data;
     wire error;
 
     // Rest of the code remains unchanged
 
+    rom #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .ADDR_WIDTH(ADDR_WIDTH),
+        .WORDS(WORDS),
+        .ENABLE_ROM_INIT(ENABLE_ROM_INIT)
+    ) dut_rom (
+        .addr_i(address),
+        .data_o(data)
+    );
+
     initial begin
+        $readmemh("init_rom.mem", dut_rom.memory);
         $dumpfile("rom_sim.vcd");
         $dumpvars(0, tb_rom);
 
@@ -19,10 +32,12 @@ module tb_rom # (
 
         // Apply addresses and observe data
         #10 address = 8'b00000000;
-        #10 address = 8'b00010000;
+        #10 address = 8'b00000001;
+        #10 address = 8'b00000010;
+        #10 address = 8'b00000011;
+        #10 address = 8'b00000100;
         // ...
 
         #100 $finish; // Allow time for observation
-        $display("Sim COMPLETE...");
     end
 endmodule
